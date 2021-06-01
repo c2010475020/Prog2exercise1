1e30361c78f19ae997063e8f7a0f0f78ff5a1ca4
Mario Ikic <71633522+c2010475020@users.noreply.github.com>
2021-06-01 21:55:03 +0200

    Initial commit

---
+++ src/test/java/passwordvalidatorTest.java
@@ -0,0 +1,77 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class passwordvalidatorTest {
+    @Test
+    public void testPasswordTooShort(){
+        boolean output = passwordvalidator.checkPassword("123");
+        assertFalse(output);
+    }
+
+    @Test
+    public void testPasswordTooLong(){
+        boolean output = passwordvalidator.checkPassword("123456789123456789123456789123456789");
+        assertFalse(output);
+    }
+
+    @Test
+    public void testPasswordValidLength(){
+        boolean output = passwordvalidator.checkPassword("1A23Hjafut@!");
+        assertTrue(output);
+    }
+
+    @Test
+    public void testPasswordContainsOnlyUppercase(){
+        boolean output = passwordvalidator.checkPassword("ABCDEFGHIJKL");
+        assertFalse(output);
+    }
+
+    @Test
+    public void testPasswordContainsOnlyLowercase(){
+        boolean output = passwordvalidator.checkPassword("abcdefghijkl");
+        assertFalse(output);
+    }
+
+    @Test
+    public void testPasswordContainsNoDigits(){
+        boolean output = passwordvalidator.checkPassword("ahgkgsdHJZF");
+        assertFalse(output);
+    }
+
+    @Test
+    public void testPasswordContainsDigitsUpperLowerSpecial(){
+        boolean output = passwordvalidator.checkPassword("ahgkgsd9H7JZF@");
+        assertTrue(output);
+    }
+
+    @Test
+    public void testPasswordContainsDigitsUpperLowerNoSpecial(){
+        boolean output = passwordvalidator.checkPassword("ahgkgsd9H7JZF");
+        assertFalse(output);
+    }
+
+    @Test
+    public void testPasswordContains4DigitsInARow(){
+        boolean output = passwordvalidator.checkPassword("ahgkgsd9999H7JZF");
+        assertFalse(output);
+    }
+
+    @Test
+    public void testPasswordContains4DigitsInARow2(){
+        boolean output = passwordvalidator.checkPassword("ahg44444kgsd99H7JZF");
+        assertFalse(output);
+    }
+
+    @Test
+    public void testPasswordContains3Digits(){
+        boolean output = passwordvalidator.checkPassword("gsadgas256712HJG$");
+        assertFalse(output);
+    }
+
+    @Test
+    public void testPasswordIsPerfect(){
+        boolean output = passwordvalidator.checkPassword("ahg44@!kGsd99H7JZF");
+        assertTrue(output);
+    }
+}
